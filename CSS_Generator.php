d0088aa9bfa826c759477a1098b09ef6d68e0080
Le Borges Geoffrey <geoffrey.leborges@epitech.eu>
2016-05-23 16:00:56 +0200

    hauter_largeur_ok

--- CSS_Generator.php
+++ CSS_Generator.php
@@ -1,33 +1,27 @@
 <?php
 
 
-function read_files($folder)
-{
+function read_files($folder){
 
     if ($handle = opendir($folder)) {
         while (false !== ($file = readdir($handle))) {
             if ($file == '.' || $file == '..') continue;
-            if (strrpos($file, '.png')) {
+            if (strrpos($file,'.png')) {
                 $tab = explode("\n", $file);
-                $toto = (list($width, $height) = getimagesize($folder . "/" . $file));
-//              echo "width: " . $width . "\n";
-//              echo "height: " .  $height . "\n";
-                $tata = array_merge($tab, $toto);
+                $toto=(list($width, $height) = getimagesize($folder . "/" . $file));
+                $tata=array_merge($tab,$toto);
                 print_r($tata);
+
+
+
+                if (is_dir($folder . "/" . $file)) {
+                    read_files($folder . "/" . $file);
+                }
+//                $tata=get_png($tab, $handle);
             }
         }
     }
 }
-
-
-/*if (is_dir($folder . "/" . $file)) {
-    read_files($folder . "/" . $file);
-}
-//                $tata=get_png($tab, $handle);
-}
-}
-}
-}*/
 read_files($argv[1]);
 
 //function get_png($tab, $handle){
