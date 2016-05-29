99acc235fe352082755138f53d4ffa7a31179559
Le Borges Geoffrey <geoffrey.leborges@epitech.eu>
2016-05-29 11:18:24 +0200

    option output image et position ok

--- CSS_Generator_2.php
+++ CSS_Generator_2.php
@@ -1,22 +1,21 @@
 <?php
 
-function output_image($argv, &$tab, &$whatIWant){
-
-    $all_args = array();
-//    $whatIWant="";
-
-    for ($i = 1; $i < count($argv); $i++) {
-        array_push($all_args, $argv[$i]);
-    }
-
-    $name_of_css = $all_args[0];
-    $whatIWant = substr($name_of_css, strpos($name_of_css, "=") + 1);
+function output_image(&$argv, &$tab, &$input=null){
 
 
+    for ($i = 0; $i < count($argv); $i++) {
+        $input = substr(strstr($argv[$i],"=" ),1 );
+        if ($input == true) {
 
-    if (!file_exists('stylesheet.css')) {
-        rename("stylesheet.css", $whatIWant .".css");
 
+            if(!file_exists('stylesheet.css')){
+                create_css($tab);
+
+            }
+            else if(file_exists('stylesheet.css')){
+                rename("stylesheet.css", $input . ".css");
+            }
+        }
     }
 }
 
@@ -24,20 +23,40 @@ function output_image($argv, &$tab, &$whatIWant){
 
 
 
-//$option="";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**************************************************************************************************
+
 //
+//<?php
 //
-//for ($i = 0; $i < count($argv); $i++) {
-//    if ($option = (strpbrk($argv[$i], "=")) && (file_exists('stylesheet.css')))  {
-//        rename("stylesheet.css", "test.css");
-//        $test=$option +1;
-//        var_dump($test) ;
+//function output_image($argv, &$tab, &$input=null){
+//
+//    for ($i = 0; $i < count($argv); $i++) {
+//        $input = strstr($argv[$i], "=");
+//        if ($input == true){
+//
+//            if (file_exists('stylesheet.css')) {
+//                rename("stylesheet.css", $input .".css");
+//
+//            } else {
+//                create_css($tab);
+//            }
+//        }
 //    }
-//
-//
-//    else if($option = (strpbrk($argv[$i], "=")) && (!file_exists('stylesheet.css'))) {
-//        create_css($tab);
-//
-//    }
-//
 //}
+//
